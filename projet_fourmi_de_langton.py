288b0bbb60fcb65a41cd55fe5ab196027eb24da4
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-04-15 10:02:27 +0200

    création fichier python

---
+++ projet_fourmi_de_langton.py
@@ -0,0 +1,8 @@
+###########################################################
+# Groupe MI4
+# Alexandre CHOLLET
+# Adam HARBANE
+# Bryan LE BLANC
+# Kais CHEBOUB
+# https://github.com/uvsq22016170/projet_fourmi_de_langton
+###########################################################
