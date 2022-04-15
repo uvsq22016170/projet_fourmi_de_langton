d83cf33eb58906be2566f1174c7019faeb37b3da
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-04-15 10:15:58 +0200

    ajout initial

--- projet_fourmi_de_langton.py
+++ projet_fourmi_de_langton.py
@@ -6,3 +6,37 @@
 # Kais CHEBOUB
 # https://github.com/uvsq22016170/projet_fourmi_de_langton
 ###########################################################
+
+###############################################################################################################################################
+#Importation des modules
+import tkinter as tk
+
+###############################################################################################################################################
+#Constantes
+#taille de base de la grille
+taille = 9
+
+###############################################################################################################################################
+#Variables globales
+
+###############################################################################################################################################
+#Fonctions
+    
+###############################################################################################################################################
+#Partie principale
+
+#Création de la fenêtre
+racine = tk.Tk()
+racine.title("Projet fourmi de langton")
+
+#Calcul du coefficient en fonction de la taille de l'écran actuel
+coeff = (min(racine.winfo_screenwidth(), racine.winfo_screenheight())/1.2) / taille
+
+#Creation des widgets
+canvas = tk.Canvas(racine, height = taille * coeff, width = taille * coeff)
+
+#Placement des widgets
+canvas.pack(side = "right")
+
+#Boucle principale
+racine.mainloop()
