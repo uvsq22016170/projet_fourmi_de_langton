7af1e8e6b3937fbfa4e198083fbe274a0c2cc61d
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-04-15 11:19:38 +0200

    ajout fonctions d'initialisation et interface

--- projet_fourmi_de_langton.py
+++ projet_fourmi_de_langton.py
@@ -10,6 +10,7 @@
 ###############################################################################################################################################
 #Importation des modules
 import tkinter as tk
+import random as rd
 
 ###############################################################################################################################################
 #Constantes
@@ -18,10 +19,37 @@ taille = 9
 
 ###############################################################################################################################################
 #Variables globales
+Coul = ["black", "white"]
 
 ###############################################################################################################################################
 #Fonctions
-    
+
+def init ():
+    """
+    Initialise la grille
+    """
+    global config, L_obj, coeff, fourmi
+    coeff = (min(racine.winfo_screenwidth(), racine.winfo_screenheight())/1.2) / taille
+    config = [[0] * taille for i in range(taille)]
+    L_obj = [[] * taille for i in range(taille)]
+    fourmi = [taille/2, taille/2, "N"]
+
+def noir ():
+    for i in range (taille) :
+        for j in range (taille) :
+            L_obj[i] += [canvas.create_rectangle((i * coeff, j * coeff), ((i+1) * coeff, (j+1) * coeff), fill = Coul[0], outline = Coul[0])]
+
+def blanc ():
+    for i in range (taille) :
+        for j in range (taille) :
+            L_obj[i] += [canvas.create_rectangle((i * coeff, j * coeff), ((i+1) * coeff, (j+1) * coeff), fill = Coul[1], outline = Coul[1])]
+
+def random ():
+    for i in range (taille):
+        for j in range (taille):
+            config[i][j] = rd.randint(0, 1)
+            canvas.itemconfigure(L_obj[i][j], fill = Coul[config[i][j]], outline = Coul[config[i][j]])
+
 ###############################################################################################################################################
 #Partie principale
 
@@ -29,14 +57,18 @@ taille = 9
 racine = tk.Tk()
 racine.title("Projet fourmi de langton")
 
-#Calcul du coefficient en fonction de la taille de l'écran actuel
-coeff = (min(racine.winfo_screenwidth(), racine.winfo_screenheight())/1.2) / taille
+#Initialisation
+init()
 
 #Creation des widgets
 canvas = tk.Canvas(racine, height = taille * coeff, width = taille * coeff)
+b_play_pause = tk.Button(racine, text = "Play")
+b_next = tk.Button(racine, text = "Faire une étape")
 
 #Placement des widgets
 canvas.pack(side = "right")
+b_play_pause.pack(side = "top", fill = "x")
+b_next.pack(side = "top", fill = "x")
 
 #Boucle principale
 racine.mainloop()
