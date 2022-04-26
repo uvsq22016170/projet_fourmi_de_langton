7ebcc31ef11ecc5aef3de07b667c904a81130ff2
Bryan LE BLANC <bryan.le-blanc@ens.uvsq.fr>
2022-04-26 15:07:38 +0200

    ajout fonctions de déplacement et retour

--- projet_fourmi_de_langton.py
+++ projet_fourmi_de_langton.py
@@ -16,11 +16,15 @@ import tkinter.messagebox as mb
 ###############################################################################################################################################
 #Constantes
 #taille de base de la grille
-taille = 9
+taille = 100
+
+#délai entre chaque étape
+delai = 0
 
 ###############################################################################################################################################
 #Variables globales
 coul = ["black", "white"]
+stop = False
 fourmi_placee = False
 execution = False
 
@@ -69,6 +73,64 @@ def blanc ():
 def aleatoire ():
     reconfiguration("r")
 
+def deplacement ():
+    global mem, execution
+    if fourmi_placee == True and execution == False:
+        mem += [[fourmi.copy(), config[fourmi[0]][fourmi[1]]]]
+        if len(mem) > 100:
+            mem = mem[1:]
+        fourmi[2] = (fourmi[2] + int(config[fourmi[0]][fourmi[1]] == 0) - int(config[fourmi[0]][fourmi[1]] == 1))%4
+        config[fourmi[0]][fourmi[1]] = int(config[fourmi[0]][fourmi[1]] == 0)
+        canvas.itemconfigure(L_obj[fourmi[0]][fourmi[1]], fill = coul[config[fourmi[0]][fourmi[1]]], outline = coul[config[fourmi[0]][fourmi[1]]])
+        fourmi[0] = int(fourmi[0] - (fourmi[2] == 0))%taille
+        fourmi[1] = int(fourmi[1] - (fourmi[2] == 1))%taille
+        fourmi[0] = int(fourmi[0] + (fourmi[2] == 2))%taille
+        fourmi[1] = int(fourmi[1] + (fourmi[2] == 3))%taille
+        canvas.itemconfigure(L_obj[fourmi[0]][fourmi[1]], fill = "red", outline = "red")
+    elif fourmi_placee == False and execution == False:
+        mb.showerror("Erreur", "Il n'y pas de fourmi dans la grille")
+    else :
+        mb.showerror("Erreur", "La fourmi avance, veuillez mettre en pause pour effectuer cette action")
+
+def play ():
+    global stop, execution
+    if fourmi_placee == True:
+        execution = True
+        b_play_pause.config(text = "Pause", command = pause)
+        while stop == False:
+            execution = False
+            deplacement()
+            execution = True
+            canvas.after(delai, racine.update())
+        execution = False
+        stop = False
+        b_play_pause.config(text = "Play", command = play)
+    else :
+        mb.showerror("Erreur", "Il n'y pas de fourmi dans la grille")
+
+def pause ():
+    global stop
+    stop = True
+
+def retour ():
+    global fourmi, mem, fourmi_placee
+    if fourmi_placee == True and execution == False:
+        if mem != [] :
+            canvas.itemconfigure(L_obj[fourmi[0]][fourmi[1]], fill = coul[config[fourmi[0]][fourmi[1]]], outline = coul[config[fourmi[0]][fourmi[1]]])
+            if mem != ["pas de fourmi"]:
+                config[mem[-1][0][0]][mem[-1][0][1]] = mem[-1][1]
+                fourmi = mem[-1][0]
+                canvas.itemconfigure(L_obj[fourmi[0]][fourmi[1]], fill = "red", outline = "red")
+            else :
+                fourmi_placee = False
+            mem = mem[:-1]
+        else :
+            mb.showerror("Erreur", "Plus de retour possible : mémoire vide")
+    elif fourmi_placee == False and execution == False:
+        mb.showerror("Erreur", "Il n'y pas de fourmi dans la grille")
+    else :
+        mb.showerror("Erreur", "La fourmi avance, veuillez mettre en pause pour effectuer cette action")
+
 ###############################################################################################################################################
 #Partie principale
 
@@ -81,8 +143,9 @@ calc_coeff ()
 
 #Creation des widgets
 canvas = tk.Canvas(racine, height = taille * coeff, width = taille * coeff)
-b_play_pause = tk.Button(racine, text = "Play")
-b_next = tk.Button(racine, text = "Faire une étape")
+b_play_pause = tk.Button(racine, text = "Play", command = play)
+b_next = tk.Button(racine, text = "Faire une étape", command = deplacement)
+b_retour = tk.Button(racine, text = "Retourner en arrière", command = retour)
 b_noir = tk.Button(racine, text = "Configuration noir", command = noir)
 b_blanc = tk.Button(racine, text = "Configuration blanc", command = blanc)
 b_aleatoire = tk.Button(racine, text = "Configuration aléatoire", command = aleatoire)
@@ -91,6 +154,7 @@ b_aleatoire = tk.Button(racine, text = "Configuration aléatoire", command = ale
 canvas.pack(side = "right")
 b_play_pause.pack(side = "top", fill = "x")
 b_next.pack(side = "top", fill = "x")
+b_retour.pack(side = "top", fill = "x")
 b_aleatoire.pack(side = "bottom", fill = "x")
 b_blanc.pack(side = "bottom", fill = "x")
 b_noir.pack(side = "bottom", fill = "x")
